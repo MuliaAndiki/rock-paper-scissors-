4765a8be5431ae03ed63ada566e23b1dec58c4a7
Mulia Andiki <muliaandiki@gmail.com>
2026-06-10 13:28:06 +0700

    fixed: docker

--- Fast/main.py
+++ Fast/main.py
@@ -16,7 +16,7 @@ from torchvision import transforms
 load_dotenv()
 
 CLASS_NAMES = ["Kertas", "Batu", "Gunting"]
-MODEL_PATH = os.getenv("MODEL_PATH", "model/model_final.pth")
+MODEL_PATH = os.getenv("MODEL_PATH", "model/model_final.pt")
 HOST = os.getenv("HOST", "0.0.0.0")
 PORT = int(os.getenv("PORT", "8000"))
 CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*").split(",")
