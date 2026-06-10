8442bc5bb81caed3226def348944cde9afbab32d
Mulia Andiki <muliaandiki@gmail.com>
2026-06-10 12:55:08 +0700

    fixed

--- Fe/.next/dev/static/chunks/src_app_page_tsx_004tgyl._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_1hucop3._.js","static/chunks/src_0qzp93x._.js"],
-    source: "dynamic"
-});
--- Fe/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"uxxPHc41cDzArJBFcQgC0lCayL7fwr1KCp5/Pi0mKCs=\"\n}"
