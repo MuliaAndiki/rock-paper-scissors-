cf27b9c8253e11f632e226fea1afd9efdd611689
Mulia Andiki <muliaandiki@gmail.com>
2026-06-10 13:11:51 +0700

    fixed:path model

--- Fast/main.py
+++ Fast/main.py
@@ -16,7 +16,7 @@ from torchvision import transforms
 load_dotenv()
 
 CLASS_NAMES = ["Kertas", "Batu", "Gunting"]
-MODEL_PATH = os.getenv("MODEL_PATH", "model/best_model.pth")
+MODEL_PATH = os.getenv("MODEL_PATH", "model/model_final.pth")
 HOST = os.getenv("HOST", "0.0.0.0")
 PORT = int(os.getenv("PORT", "8000"))
 CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*").split(",")
